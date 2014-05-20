26eef2e6efae3fd111eaab9789bb0505c96baff7
Vitor Leal <vitorleal1@gmail.com>
2014-05-20 13:07:05 -0300

    add public variable to the routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,43 @@
 var light = angular.module('light', ['ngRoute', 'light.services', 'light.controllers']);
 
+
+light.run( function($rootScope, $location, Local) {
+  $rootScope.$on('$routeChangeStart', function(event, next, current) {
+    var user = Local.get('user');
+
+    if (!next.public && !user) {
+      $location.path('login');
+    }
+  });
+});
+
+
 light.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'views/login.html',
-      controller : 'Login'
+      controller : 'Login',
+      public     : true
     })
     //Dashboard
     .when('/dashboard', {
       templateUrl: 'views/dashbard.html',
       controller : 'Dashbard',
+      public: false,
       resolve: {
         balance: ['Local', 'LoginService', '$rootScope', function (Local, LoginService, $rootScope) {
           var user = Local.get('user');
 
-          LoginService.send({
-            email: user.email,
-            pass : user.pass
-          })
-          .success(function (data) {
-            Local.set('user', data);
-            $rootScope.user = data;
-          });
+          if (user) {
+            LoginService.send({
+              email: user.email,
+              pass : user.pass
+            })
+            .success(function (data) {
+              Local.set('user', data);
+              $rootScope.user = data;
+            });
+          }
         }]
       }
     })
@@ -29,18 +45,22 @@ light.config(['$routeProvider', '$locationProvider', function ($routeProvider, $
     .when('/credit', {
       templateUrl: 'views/credit.html',
       controller : 'Credit',
+      public: false,
       resolve: {
         user: ['Local', 'LoginService', '$rootScope', function (Local, LoginService, $rootScope) {
           var user = Local.get('user');
 
-          LoginService.send({
-            email: user.email,
-            pass : user.pass
-          })
-          .success(function (data) {
-            Local.set('user', data);
-            $rootScope.user = data;
-          });
+          if (user) {
+            LoginService.send({
+              email: user.email,
+              pass : user.pass
+            })
+            .success(function (data) {
+              Local.set('user', data);
+              $rootScope.user = data;
+            });
+
+          }
         }]
       }
     })
